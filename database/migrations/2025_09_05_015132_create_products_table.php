fac324f565c8c919114471ac0ec8b1eaa2aed86b
makhi031 <mertisepic031@gmail.com>
2025-09-05 09:57:36 +0800

    + category column -> products table

--- database/migrations/2025_09_05_015132_create_products_table.php
+++ database/migrations/2025_09_05_015132_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->string('category');
             $table->text('image_path');
             $table->integer('price');
             $table->integer('stock');
